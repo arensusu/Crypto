899c6144ac758f37303d8f2b0298b1019e5e8edc
Susu Ko <willy9966@gmail.com>
2023-01-18 15:45:18 +0800

    erc20, test and deploy

--- contracts/SusuToken.sol
+++ contracts/SusuToken.sol
@@ -2,29 +2,63 @@
 pragma solidity ^0.8.10;
 
 contract SusuToken {
-    address private owner;
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
-    uint256 private _totalSupply;
-    mapping(address => uint256) private _balanceOf;
+    // Token metadata
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
 
+    // Mapping of addresses to token balances
+    mapping(address => uint256) public balanceOf;
+
+    // Event for token transfers
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // Initialize the contract with the token metadata
     constructor() {
-        _name = "SusuToken";
-        _symbol = "SST";
-        _decimals = 9;
-        _totalSupply = 1000000000000;
-        owner = msg.sender;
+        name = "SusuToken";
+        symbol = "SST";
+        decimals = 9;
+        totalSupply = 1 ether;
+        balanceOf[msg.sender] = totalSupply;
     }
 
-    function name() public view returns (string memory) { return _name; }
-    function symbol() public view returns (string memory) { return _symbol; }
-    function decimals() public view returns (uint8) { return _decimals; }
-    function totalSupply() public view returns (uint256) { return _totalSupply; }
-    function balanceOf(address _owner) public view returns (uint256 balance) { return _balanceOf[_owner]; }
-    function transfer(address _to, uint256 _value) public returns (bool success) {}
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
-    function approve(address _spender, uint256 _value) public returns (bool success) {}
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {}
+    // Transfer tokens from one address to another
+    function transfer(address to, uint256 value) public {
+        require(balanceOf[msg.sender] >= value && value > 0);
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+    }
 
+    // Approve another address to transfer tokens on behalf of the msg.sender
+    function approve(address spender, uint256 value) public {
+        require(balanceOf[msg.sender] >= value && value > 0);
+        require(spender != address(0));
+        address owner = msg.sender;
+        _approve(owner, spender, value);
+    }
+
+    // Transfer tokens from one address to another using an approved allowance
+    function transferFrom(address from, address to, uint256 value) public {
+        require(balanceOf[from] >= value && value > 0);
+        require(_allowance[from][msg.sender] >= value);
+        balanceOf[from] -= value;
+        _allowance[from][msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+    }
+
+    // Get the approved allowance for an address
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowance[owner][spender];
+    }
+
+    mapping(address => mapping(address => uint256)) private _allowance;
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        _allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_KEY,
+      accounts: [WALLET_PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  }
 };
